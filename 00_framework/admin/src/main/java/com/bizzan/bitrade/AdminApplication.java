853f2af86c4cee19d01faa3b1c43214cd00f0012
zlq4863947 <zlq4863947@gmail.com>
2021-07-21 23:49:48 +0900

    first commit

---
+++ 00_framework/admin/src/main/java/com/bizzan/bitrade/AdminApplication.java
@@ -0,0 +1,16 @@
+package com.bizzan.bitrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author Hevin QQ:390330302 E-mail:xunibidev@gmail.com
+ */
+@SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy=true)
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
